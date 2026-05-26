23884feb3728d21a682abeec8c96bd80a6d189b7
beachboy9527 <59339713+beachboy9527@users.noreply.github.com>
2026-05-26 20:50:00 +0800

    Add OpenClash feed source to feeds.conf.default

--- diy-part1.sh
+++ diy-part1.sh
@@ -15,6 +15,6 @@
 sed -i '1i src-git openclash https://github.com/vernesong/openclash' feeds.conf.default
 
 # Add a feed source
-#echo 'src-git openclash https://github.com/vernesong/OpenClash' >>feeds.conf.default
 echo 'src-git helloworld https://github.com/fw876/helloworld.git' >>feeds.conf.default
 #echo 'src-git passwall https://github.com/xiaorouji/openwrt-passwall' >>feeds.conf.default
+echo 'src-git openclash https://github.com/vernesong/OpenClash' >>feeds.conf.default
